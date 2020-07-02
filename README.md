7b65405d2881378cb3ea80038f209e77e8e4597d
allanwelerson <allanwelerson@gmail.com>
2020-07-02 02:05:45 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+E-mail no laravel, seguindo tutorial do youtube da Code Experts e outros
---
+++ resources/views/emails/user-registered.blade.php
@@ -0,0 +1,8 @@
+<h1>Olá, {{$user->name}}, Obrigado por sua inscrição!</h1>
+
+<p>
+    Seu e-mail cadastrado é: <strong>{{$user->email}}</strong>
+</p>
+<hr>
+Email enviado em {{date('d/m/Y H:i:s')}}
+
